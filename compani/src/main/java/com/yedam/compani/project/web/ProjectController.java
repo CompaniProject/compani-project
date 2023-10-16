9763aac3386c7ffb24ee07112e63a3316c130223
gyowoo1113 <yereube@naver.com>
2023-10-16 20:36:19 +0900

    Add : front 작업을 위한 project-home html, controller 멤버함수 추가

--- compani/src/main/java/com/yedam/compani/project/web/ProjectController.java
+++ compani/src/main/java/com/yedam/compani/project/web/ProjectController.java
@@ -1,8 +1,12 @@
 package com.yedam.compani.project.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProjectController {
-
+	@GetMapping("/project/home")
+	public String projectHome() {
+		return "project/project-home";
+	}
 }
