dd48bb078a0fbcdfebae7b5335540d57af988a3c
gyowoo113 <yereube@naver.com>
2023-10-17 16:42:54 +0900

    Feat : scheduler annotation 추가, 회사별 월 통계 scheduler class 추가
     fixedDelay = 1000로 scheduler 실행 테스트 진행 - 문제없이 동작함

--- compani/src/main/java/com/yedam/compani/CompaniApplication.java
+++ compani/src/main/java/com/yedam/compani/CompaniApplication.java
@@ -1,10 +1,13 @@
 package com.yedam.compani;
 
 import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableAspectJAutoProxy
 @SpringBootApplication
 @MapperScan(basePackages = "com.yedam.compani.**.mapper")
---
+++ compani/src/main/java/com/yedam/compani/company/status/scheduler/CompanyStatusScheduler.java
@@ -0,0 +1,14 @@
+package com.yedam.compani.company.status.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyStatusScheduler {
+
+	// 매달 1일 오전 4시에 동작
+    @Scheduled(cron = "0 0 4 1 * *", zone = "Asia/Seoul")
+    public void calculateCompanyStatus(){
+    	// insert status data - per company, to project_status
+    }
+}
