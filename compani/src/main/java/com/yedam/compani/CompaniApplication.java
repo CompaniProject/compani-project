3ec94149fda2381f04c638c222feb02a4c654350
gyowoo113 <yereube@naver.com>
2023-10-17 16:20:09 +0900

    Setting : aop depandancy, annotation 추가

--- compani/src/main/java/com/yedam/compani/CompaniApplication.java
+++ compani/src/main/java/com/yedam/compani/CompaniApplication.java
@@ -3,7 +3,9 @@ package com.yedam.compani;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 @MapperScan(basePackages = "com.yedam.compani.**.mapper")
 public class CompaniApplication {
