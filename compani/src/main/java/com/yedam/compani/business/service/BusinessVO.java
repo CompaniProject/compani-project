f9ed9ac3793415002b443d86da95cfb4e5ce67cd
MyNameIsYGKim <kelen536@gmail.com>
2023-10-17 17:29:01 +0900

    10/17 17:28 캘린더 화면 작업중

---
+++ compani/src/main/java/com/yedam/compani/business/service/BusinessVO.java
@@ -0,0 +1,19 @@
+package com.yedam.compani.business.service;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BusinessVO {
+	private int bussNo;
+	private int prjtNo;
+	private int bussUpcd;
+	private String bussNm;
+	private String bussImp;
+	private int bussPrgre;
+	private Date bussFrdt;
+	private Date bussTodt;
+	private Date bussCmpltdt;
+	private String bussSt;
+}
