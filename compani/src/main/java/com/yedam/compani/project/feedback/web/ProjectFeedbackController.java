3d94b6c26160676461496ab2b35956c77fac53db
gyowoo113 <yereube@naver.com>
2023-10-17 14:35:20 +0900

    Add : add project feedback Controller, html

--- compani/src/main/java/com/yedam/compani/project/feedback/web/ProjectFeedbackController.java
+++ compani/src/main/java/com/yedam/compani/project/feedback/web/ProjectFeedbackController.java
@@ -1,8 +1,13 @@
 package com.yedam.compani.project.feedback.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProjectFeedbackController {
-
+	// url 수정 필요
+	@GetMapping("/project/feedback")
+	public String projectFeedbackHome() {
+		return "project/feedback-home";
+	}
 }
