6efab140b4fac1cd04c87aaa7f065c17e0493bea
gyowoo1113 <yereube@naver.com>
2023-10-18 21:01:59 +0900

    Feat #3 : project member select에 대한 mapper, service/impl 추가

--- compani/src/main/java/com/yedam/compani/project/member/service/ProjectMemberService.java
+++ compani/src/main/java/com/yedam/compani/project/member/service/ProjectMemberService.java
@@ -1,10 +1,11 @@
 package com.yedam.compani.project.member.service;
 
 import java.util.List;
-
+import java.util.Map;
 
 
 public interface ProjectMemberService {
 	
 	public List<ProjectMemberVO> getProjectMemberCountList();
+	public List<Map<Object,Object>> getProjectMemberList(Integer prjtNo);
 }
