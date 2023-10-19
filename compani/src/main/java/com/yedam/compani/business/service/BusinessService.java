278b51e52761a5db14deddf8a623bda7d478e6ad
gyowoo113 <yereube@naver.com>
2023-10-19 14:31:13 +0900

    Feat #5 : pie chart를 위해 map 대신 list 형식으로 key, value return 하도록 변경

--- compani/src/main/java/com/yedam/compani/business/service/BusinessService.java
+++ compani/src/main/java/com/yedam/compani/business/service/BusinessService.java
@@ -10,6 +10,6 @@ public interface BusinessService {
 	// 프로젝트 캘린더 업무리스트
 	public List<BusinessVO> getProjectCalenderBusinessList();
 
-	public List<Map<Object,Object>> getBusinessStateList(Integer prjtNo);
+	public List<List<String>> getBusinessStateList(Integer prjtNo);
 	public List<Map<Object,Object>> getBusinessAndLevelList(Integer prjtNo);
 }
