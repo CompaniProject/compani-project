d51e24ddfbefe207f8ae1a2b89d52aa1c6bf56a8
MyNameIsYGKim <kelen536@gmail.com>
2023-10-19 17:53:49 +0900

    10/19 17:53 캘린더 업무바 구현중

--- compani/src/main/java/com/yedam/compani/calendar/CalendarController.java
+++ compani/src/main/java/com/yedam/compani/calendar/CalendarController.java
@@ -2,6 +2,8 @@ package com.yedam.compani.calendar;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.yedam.compani.business.service.BusinessService;
 import com.yedam.compani.business.service.BusinessVO;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class CalendarController {
+	
+	private static final Logger log = LoggerFactory.getLogger(CalendarController.class);
 	
 	@Autowired
 	BusinessService businessService;
