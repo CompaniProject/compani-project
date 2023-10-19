d68e5debc34282778c492f3f6896048050a82f8e
gyowoo113 <yereube@naver.com>
2023-10-19 10:03:01 +0900

    Test, Add #4 : businessStateList 결과 출력하는 Test method 추가 및 테스트

---
+++ compani/src/test/java/com/yedam/compani/business/service/impl/BusinessServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.yedam.compani.business.service.impl;
+
+import com.yedam.compani.business.service.BusinessService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BusinessServiceImplTest {
+
+    @Autowired
+    BusinessService businessService;
+
+    @Test
+    void getBusinessStateList() {
+        List<Map<Object,Object>> stateList = businessService.getBusinessStateList(1);
+        System.out.println(stateList);
+    }
+}
