46e78582eed5215f4c9ad89a69de95601bcd2c33
gyowoo113 <yereube@naver.com>
2023-10-19 10:02:16 +0900

    Feat #4 : 프로젝트에 대한 업무 상태별 갯수 구하는 query 및 service/mapper/impl 추가

--- compani/src/main/java/com/yedam/compani/business/service/impl/BusinessServiceImpl.java
+++ compani/src/main/java/com/yedam/compani/business/service/impl/BusinessServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yedam.compani.business.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,11 @@ public class BusinessServiceImpl implements BusinessService {
 	@Override
 	public List<BusinessVO> getProjectCalenderBusinessList() {
 		return businessMapper.selectProjectCalendarAllBusiness();
+	}
+
+	@Override
+	public List<Map<Object, Object>> getBusinessStateList(Integer prjtNo) {
+		return businessMapper.selectBusinessStateList(prjtNo);
 	}
 
 }
