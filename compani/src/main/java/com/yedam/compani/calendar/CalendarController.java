dc7c7f64fe2da42793ea1d74679b04eb4849679b
MyNameIsYGKim <kelen536@gmail.com>
2023-10-20 16:05:01 +0900

    10/20 16:04 캘린더 정렬 작업중
    10/20 16:04 캘린더 정렬 작업중

--- compani/src/main/java/com/yedam/compani/calendar/CalendarController.java
+++ compani/src/main/java/com/yedam/compani/calendar/CalendarController.java
@@ -2,8 +2,6 @@ package com.yedam.compani.calendar;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,8 +15,6 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class CalendarController {
-	
-	private static final Logger log = LoggerFactory.getLogger(CalendarController.class);
 	
 	@Autowired
 	BusinessService businessService;
@@ -39,3 +35,14 @@ public class CalendarController {
 		return "calendar/projectCalendarPage";
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
