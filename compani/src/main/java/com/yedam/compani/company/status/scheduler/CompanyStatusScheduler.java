843c4a471e498ac80175ffe6bc4595f7eb3f6276
gyowoo113 <yereube@naver.com>
2023-10-20 12:57:49 +0900

    Remove : scheduler zone 제거

--- compani/src/main/java/com/yedam/compani/company/status/scheduler/CompanyStatusScheduler.java
+++ compani/src/main/java/com/yedam/compani/company/status/scheduler/CompanyStatusScheduler.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class CompanyStatusScheduler {
 
 	// 매달 1일 오전 4시에 동작
-    @Scheduled(cron = "0 0 4 1 * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 4 1 * *")
     public void calculateCompanyStatus(){
     	// insert status data - per company, to project_status
     }
