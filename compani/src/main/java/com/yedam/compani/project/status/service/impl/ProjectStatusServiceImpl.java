09a305933f4d81b09afbc89f9af9121c089deb34
gyowoo113 <yereube@naver.com>
2023-10-23 15:33:53 +0900

    Feat #1 , #6 : Service Annotation 추가, insert query 오타 수정

--- compani/src/main/java/com/yedam/compani/project/status/service/impl/ProjectStatusServiceImpl.java
+++ compani/src/main/java/com/yedam/compani/project/status/service/impl/ProjectStatusServiceImpl.java
@@ -3,7 +3,9 @@ package com.yedam.compani.project.status.service.impl;
 import com.yedam.compani.project.status.mapper.ProjectStatusMapper;
 import com.yedam.compani.project.status.service.ProjectStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProjectStatusServiceImpl implements ProjectStatusService {
 
     @Autowired
