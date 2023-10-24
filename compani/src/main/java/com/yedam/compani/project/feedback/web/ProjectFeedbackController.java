e081a5b40d0c647b1d942b7326ee93303d835c65
gyowoo113 <yereube@naver.com>
2023-10-24 15:43:37 +0900

    Feat : project feedback controller에 companyStatus info 추가

--- compani/src/main/java/com/yedam/compani/project/feedback/web/ProjectFeedbackController.java
+++ compani/src/main/java/com/yedam/compani/project/feedback/web/ProjectFeedbackController.java
@@ -1,6 +1,9 @@
 package com.yedam.compani.project.feedback.web;
 
+import com.yedam.compani.company.status.service.CompanyStatusService;
+import com.yedam.compani.company.status.service.CompanyStatusVO;
 import com.yedam.compani.project.status.service.ProjectStatusVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,15 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.yedam.compani.project.status.service.ProjectStatusService;
 
 @Controller
+@RequiredArgsConstructor
 public class ProjectFeedbackController {
-	
-	@Autowired
-	ProjectStatusService projectStatusService;
+
+	private final ProjectStatusService projectStatusService;
+	private final CompanyStatusService companyStatusService;
 	
 	@GetMapping("/project/feedback/{prjtNo}")
 	public String projectFeedbackHome(@PathVariable int prjtNo, Model model) {
+		// get session users company data
+		String coCd = "CPN001";
+
 		ProjectStatusVO projectStatus = projectStatusService.getProjectStatus(prjtNo);
+		CompanyStatusVO cpnStatForCurrDt = companyStatusService.getStatusForCurrentDate(coCd);
+		CompanyStatusVO cpnStatForPrjtDt = companyStatusService.getStatusForProjectDate(prjtNo,coCd);
+
 		model.addAttribute("projectStatus",projectStatus);
+		model.addAttribute("cpnStatForCurrDt",cpnStatForCurrDt);
+		model.addAttribute("cpnStatForPrjtDt",cpnStatForPrjtDt);
 		return "project/feedback-home";
 	}
 }
