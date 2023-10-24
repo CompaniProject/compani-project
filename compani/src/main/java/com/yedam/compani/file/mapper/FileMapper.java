aaa71ad135cebe119d8300a5f9125cc31e520103
jack2078 <jack2078@naver.com>
2023-10-24 17:46:26 +0900

    업무 모달 파일삭제 AJAX
    단건삭제 ajax 구현
    전체,선택삭제 95% 완성 (ajax)
    mapper service쪽 대문자 -> 소문자 수정

--- compani/src/main/java/com/yedam/compani/file/mapper/FileMapper.java
+++ compani/src/main/java/com/yedam/compani/file/mapper/FileMapper.java
@@ -7,18 +7,21 @@ import com.yedam.compani.file.service.FileVO;
 public interface FileMapper {
 
 		// 전체보기
-		public List<FileVO> FileList();
+		public List<FileVO> fileList();
 		
 		// 조회
-		public FileVO FileInfo(FileVO fileVO);
+		public FileVO fileInfo(FileVO fileVO);
 		
 		// 등록
-		public int FileInsert(FileVO fileVO);
+		public int fileInsert(FileVO fileVO);
 		
 		// 삭제
-		public int FileDelete(Integer FileNo);
+		public int fileDelete(Integer fileNo);
+		
+		// 선택삭제
+		public int fileSelDel(List<Integer> fileNo);
 		
 		// 업무 모달 파일함 검색기능
-		public List<FileVO> FileSearch(FileVO fileVO);
+		public List<FileVO> fileSearch(FileVO fileVO);
 	
 }
--- compani/src/main/java/com/yedam/compani/file/web/FileDeleteController.java
+++ compani/src/main/java/com/yedam/compani/file/web/FileDeleteController.java
@@ -1,15 +1,10 @@
 package com.yedam.compani.file.web;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,22 +17,17 @@ public class FileDeleteController {
 	FileService fileservice;
 	
 	// 파일 삭제 - 하나삭제 - AJAX
-		@GetMapping("getDelAjax")
+		@GetMapping("/fileOneDelete")
 		@ResponseBody // AJAX 사용
-		public Map<String, Object> fileDelete(@RequestParam Integer fileNo) {
-			List<Integer> list = new ArrayList<>(); // 리스트에 몇개가 있든 상관없이 담음
-			list.add(fileNo); // list에다 파일번호 삽입
-			return fileservice.getFileDelete(list);
+		public int fileOneDelete(Integer fileNo) {
+			return fileservice.fileDelete(fileNo);
 		}
 
-		/*
-		 * // 파일 삭제 - 선택삭제 - AJAX
-		 * 
-		 * @PostMapping("jsonDelAjax")
-		 * 
-		 * @ResponseBody // JSON 사용 public boolean empListDelete(@RequestBody
-		 * List<Integer> workFile) { Map<String, Object> map = new HashMap<>();
-		 * map.put("result", true); map.put("data", workFile); return (boolean) // 성공 여부
-		 * 리턴 }
-		 */
+	// 파일 삭제 - 체크박스 - 전체선택 || 선택삭제 - AJAX
+		@GetMapping("/fileSelectDel")
+		@ResponseBody
+		public int fileSelectDel(@RequestParam List<Integer> files) {
+			System.out.println(files);
+			return fileservice.fileSelDel(files);
+		}
 }
