b79a8bf38145d74a192ee2dec9fd5fae5e38fc99
gyowoo113 <yereube@naver.com>
2023-10-24 15:30:58 +0900

    Feat #6 : 프로젝트 날짜기준, 현재 날짜기준 회사 통계 select 하는 query 및 mapper/service/impl 추가

--- compani/src/main/java/com/yedam/compani/company/status/service/CompanyStatusService.java
+++ compani/src/main/java/com/yedam/compani/company/status/service/CompanyStatusService.java
@@ -4,4 +4,6 @@ import java.util.Date;
 
 public interface CompanyStatusService {
     public void insert(Date date);
+    public CompanyStatusVO getStatusForCurrentDate(String coCd);
+    public CompanyStatusVO getStatusForProjectDate(int prjtNo, String coCd);
 }
--- compani/src/main/java/com/yedam/compani/company/status/service/impl/CompanyStatusServiceImpl.java
+++ compani/src/main/java/com/yedam/compani/company/status/service/impl/CompanyStatusServiceImpl.java
@@ -4,6 +4,7 @@ import com.yedam.compani.company.status.mapper.CompanyStatusMapper;
 import com.yedam.compani.company.status.service.CompanyStatusService;
 import com.yedam.compani.company.status.service.CompanyStatusVO;
 import com.yedam.compani.project.service.ProjectService;
+import com.yedam.compani.project.service.ProjectVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,19 @@ import java.util.Date;
 public class CompanyStatusServiceImpl implements CompanyStatusService {
 
     private final CompanyStatusMapper companyStatusMapper;
-    private final ProjectService projectService;
 
     @Override
     public void insert(Date date) {
         companyStatusMapper.insert(date);
+    }
+
+    @Override
+    public CompanyStatusVO getStatusForCurrentDate(String coCd) {
+        return companyStatusMapper.selectStatusForCurrentDate(coCd);
+    }
+
+    @Override
+    public CompanyStatusVO getStatusForProjectDate(int prjtNo, String coCd) {
+        return companyStatusMapper.selectStatusForProjectDate(prjtNo,coCd);
     }
 }
