aec5bbefd34adc1393496b889d14dbb59bbeb22d
jack2078 <jack2078@naver.com>
2023-10-25 18:11:19 +0900

    삭제완료 + 검색 중
    전체 선택 단건 삭제 + 검색 기능 구현중

---
+++ compani/src/main/java/com/yedam/compani/file/service/FileSearch.java
@@ -0,0 +1,8 @@
+package com.yedam.compani.file.service;
+
+import lombok.Data;
+
+@Data
+public class FileSearch {
+	private String keyword;
+}
--- compani/src/main/java/com/yedam/compani/file/service/FileVO.java
+++ compani/src/main/java/com/yedam/compani/file/service/FileVO.java
@@ -17,5 +17,8 @@ public class FileVO {
 	private Date fileDt;
 	private String membId;
 	private Integer fileSize;
+	
+	private String type;
+	private String keyword;
 
 }
