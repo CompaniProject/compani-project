44f0f21ce9b497943c6daa62cd389f718a874868
gyowoo113 <yereube@naver.com>
2023-10-27 12:54:31 +0900

    Refactor #7 : 통계 관련 script 분리

---
+++ compani/src/main/resources/static/js/project-feedback-chart.js
@@ -0,0 +1,141 @@
+/**
+ * 
+ */
+ 
+window.onload = function () {
+    donutBussChartDraw();
+    donutIssueChartDraw();
+    stackBussChartDraw();
+    stackIssueChartDraw();
+}
+ 
+
+//---------------------- Business Doughnut Chart Start ------
+let pieBussChartData = {
+	
+    labels: ['완료', '미완료', '초과완료'],
+    datasets: [{
+        data: [projectStatus.bussCmplCnt, projectStatus.bussUncmplCnt, projectStatus.bussExceCnt],
+        backgroundColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)']
+    }] 
+};
+
+let donutBussChartDraw = function() {
+    let ctx = document.getElementById('bussDonutChart').getContext('2d');
+    
+    window.pieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: pieBussChartData,
+        options: {
+            responsive: false
+        }
+    });
+};
+//---------------------- Business Doughnut Chart End ------
+
+let barOption = {
+    	plugins:{
+    		stacked100 :{
+    			enable: true
+    		}
+    	},
+        responsive: false,
+        scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        },
+    }
+
+// ----------------------Business Bar Chart Start -------
+let stackBussChartDraw = function(){
+	
+	var stctx = document.getElementById("bussStBar");
+    window.stBar = new Chart(stctx, { 
+        type: 'bar', 
+        data: { 
+            labels: ["프로젝트", "회사(프로젝트 완료시점)","회사(현재)"],
+            datasets: [{ 
+                label: '완료', 
+                backgroundColor: 'rgb(255, 99, 132)', 
+                data: [projectStatus.bussCmplRate,
+                	cpnStatForPrjtDt.mmBussCmplRate,
+                	cpnStatForCurrDt.mmBussCmplRate], 
+            }, { 
+                label: '미완료', 
+                backgroundColor: 'rgb(255, 159, 64)', 
+                data: [projectStatus.bussUncmplRate,
+                	cpnStatForPrjtDt.mmBussUncmplRate,
+                	cpnStatForCurrDt.mmBussUncmplRate], 
+            }, { 
+                label: '초과완료', 
+                backgroundColor: 'rgb(255, 205, 86)', 
+                data: [projectStatus.bussExceRate,
+                	cpnStatForPrjtDt.mmBussExcecmplRate,
+                	cpnStatForCurrDt.mmBussExcecmplRate], 
+            }], 
+        }, 
+        options: barOption
+    });
+}
+
+//----------------------Business Bar Chart End -------
+
+//----------------------Issue Doughnut Chart Start
+let pieIssuChartData = {
+	
+    labels: ['해결', '미해결', '해결불가'],
+    datasets: [{
+        data: [projectStatus.issuSolveCnt, projectStatus.issuUnsolveCnt, projectStatus.issuUnsolvableCnt],
+        backgroundColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)']
+    }] 
+};
+
+let donutIssueChartDraw = function() {
+    let ctx = document.getElementById('issuDonutChart').getContext('2d');
+    
+    window.pieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: pieIssuChartData,
+        options: {
+            responsive: false
+        }
+    });
+};
+
+//----------------------Issue Doughnut Chart Start End
+
+// ----------------------Issue Bar Chart Start -------
+let stackIssueChartDraw = function(){
+	
+	var stctx = document.getElementById("issuStBar");
+    window.stBar = new Chart(stctx, { 
+        type: 'bar', 
+        data: { 
+            labels: ["프로젝트", "회사(프로젝트 완료시점)","회사(현재)"],
+            datasets: [{ 
+                label: '완료', 
+                backgroundColor: 'rgb(255, 99, 132)', 
+                data: [projectStatus.issuSolveRate,
+                	cpnStatForPrjtDt.mmIssuSolveRate,
+                	cpnStatForCurrDt.mmIssuSolveRate], 
+            }, { 
+                label: '미완료', 
+                backgroundColor: 'rgb(255, 159, 64)', 
+                data: [projectStatus.issuUnsolveRate,
+                	cpnStatForPrjtDt.mmIssuUnsolveRate,
+                	cpnStatForCurrDt.mmIssuUnsolveRate], 
+            }, { 
+                label: '초과완료', 
+                backgroundColor: 'rgb(255, 205, 86)', 
+                data: [projectStatus.issuUnsolvableRate,
+                	cpnStatForPrjtDt.mmIssuUnsolvableRate,
+                	cpnStatForCurrDt.mmIssuUnsolvableRate], 
+            }], 
+        }, 
+        options: barOption
+    });
+}
