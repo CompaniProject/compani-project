df12e07e83fe3f4c00b9cb599310d6ca14cd3341
eocjf324 <57933747+eocjf324@users.noreply.github.com>
2023-11-06 14:37:25 +0900

    업무 모달 작업중

--- compani/src/main/java/com/yedam/compani/business/web/BusinessController.java
+++ compani/src/main/java/com/yedam/compani/business/web/BusinessController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -79,7 +80,7 @@ public class BusinessController {
 	}
 
 	@GetMapping("/businessInfo/{bussNo}")
-	public String businessInfo(@PathVariable Integer bussNo ,Model model) {
+	public String businessInfo(@PathVariable Integer bussNo ,Model model, HttpServletRequest request) {
 
 		// 업무 단건
 		BusinessVO bussVO = businessService.businessSelect(bussNo);
@@ -92,6 +93,15 @@ public class BusinessController {
 		// 회사 멤버 list
 		List<MemberVO> memberList = memberService.getMemberList();
 		model.addAttribute("memberList", memberList);
+		
+		int prjtNo = (int) request.getSession().getAttribute("prjtNo");
+		System.out.println(prjtNo);
+		/*
+		 * List<BusinessVO> busineesNameList =
+		 * businessService.bussinessNameList(prjtNo);
+		 * model.addAttribute("businessNameList", busineesNameList);
+		 */
+		
 
 		return "modal/modal-business";
 	}
@@ -135,14 +145,15 @@ public class BusinessController {
 	public Map<String, Object> updateBusiness(@RequestBody FormVO formVO) {
 
 		Map<String, Object> map = new HashMap<>();
-		
+		businessService.modifyBusiness(formVO.getBusiness());
 		//새로운 Service 만들기
-		if (!formVO.getBusiness().getBussDep().equals("")) {
-			businessService.updateBusiness(formVO.getBusiness());
-		}
+		/*
+		 * if (!formVO.getBusiness().getBussDep().equals("")) {
+		 * businessService.modifyBusiness(formVO.getBusiness()); }
+		 */
 	
-		businessMemberService.deleteBusinessMember(formVO);
-		businessMemberService.insertBusinessMember(formVO);
+		//businessMemberService.deleteBusinessMember(formVO);
+		//businessMemberService.insertBusinessMember(formVO);
 		 
 		
 		return map;
