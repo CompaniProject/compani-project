1111354f18311deef00ad0aea3564c48e4d28119
GIRIBOY <wkd3795@hanmail.net>
2023-11-06 18:37:08 +0900

    개인피드백CSS수정

--- compani/src/main/java/com/yedam/compani/business/service/impl/BusinessServiceImpl.java
+++ compani/src/main/java/com/yedam/compani/business/service/impl/BusinessServiceImpl.java
@@ -18,13 +18,13 @@ import lombok.RequiredArgsConstructor;
 public class BusinessServiceImpl implements BusinessService {
 
 	private final BusinessMapper businessMapper;
-	
+
 	// 개인캘린더 업무리스트
 	@Override
-	public List<Map<Object,Object>> getPersonalCalendarBusinessList(String membId) {
+	public List<Map<Object, Object>> getPersonalCalendarBusinessList(String membId) {
 		return businessMapper.selectPersonalCalendarAllBusiness(membId);
 	}
-	
+
 	// 프로젝트 캘린더 업무리스트
 	@Override
 	public List<BusinessVO> getProjectCalenderBusinessList(Integer prjtNo) {
@@ -34,11 +34,11 @@ public class BusinessServiceImpl implements BusinessService {
 	@Override
 	public List<List<String>> getBusinessStateList(Integer prjtNo) {
 		List<List<String>> stateInfo = new ArrayList<>();
-		List<Map<Object,Object>> stateList = businessMapper.selectBusinessStateList(prjtNo);
+		List<Map<Object, Object>> stateList = businessMapper.selectBusinessStateList(prjtNo);
 		List<String> states = new ArrayList<String>();
 		List<String> stateCnts = new ArrayList<String>();
 
-		for (Map<Object,Object> info: stateList) {
+		for (Map<Object, Object> info : stateList) {
 			states.add((String) info.get("bussSt"));
 			stateCnts.add(String.valueOf(info.get("bussStCnt")));
 		}
@@ -58,7 +58,7 @@ public class BusinessServiceImpl implements BusinessService {
 	public int updateProceedToDelay() {
 		return businessMapper.updateProceedToDelay();
 	}
-	
+
 	@Override
 	public List<BusinessVO> getBusinessList(MemberVO memberVO) {
 		return businessMapper.getBusinessList(memberVO);
@@ -66,35 +66,35 @@ public class BusinessServiceImpl implements BusinessService {
 
 	@Override
 	public List<BusinessVO> bussinessNameList(Integer prjtNo) {
-		
+
 		return businessMapper.businessNameList(prjtNo);
 	}
 
 	@Override
 	public int insertBusiness(BusinessVO businessVO) {
-		
-		return 	businessMapper.insertBusiness(businessVO);
+
+		return businessMapper.insertBusiness(businessVO);
 	}
 
 	@Override
 	public int updateBusiness(BusinessVO businessVO) {
-		
+
 		return businessMapper.updateBusiness(businessVO);
-		
+
 	}
 
 	@Override
 	public BusinessVO businessSelect(Integer bussNo) {
-		
+
 		return businessMapper.businessSelect(bussNo);
 	}
-	
+
 	// 캘린더&간트 업무바 수정
 	@Override
 	public int updateCalendarBuss(BusinessVO vo) {
 		return businessMapper.updateCalendarBuss(vo);
 	}
-	
+
 	// 간트 상위업무 수정
 	@Override
 	public int updateGanttUpcd(BusinessVO vo) {
@@ -103,10 +103,13 @@ public class BusinessServiceImpl implements BusinessService {
 
 	@Override
 	public int modifyBusiness(BusinessVO businessVO) {
-		
+
 		return businessMapper.modifyBusiness(businessVO);
+	}
+
+	@Override
 	public List<BusinessVO> businessdriveNameList(Integer prjtNo) {
-		
+
 		return businessMapper.businessdriveNameList(prjtNo);
 	}
 
