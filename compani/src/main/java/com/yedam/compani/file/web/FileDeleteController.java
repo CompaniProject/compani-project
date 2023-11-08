e22c32aa83855abf3175ce4126f6ee22aed959f4
MKHHHHHHH <jack2078@naver.com>
2023-11-08 21:23:36 +0900

    검색 & 필터링 이미지 출력 / 선택삭제
    필터링 속 이미지 출력 하다가 포기
    (ul li로 꾸미기 힘들어보임)

--- compani/src/main/java/com/yedam/compani/file/web/FileDeleteController.java
+++ compani/src/main/java/com/yedam/compani/file/web/FileDeleteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,7 +25,7 @@ public class FileDeleteController {
 		}
 
 	// 파일 삭제 - 체크박스 - 전체선택 || 선택삭제 - AJAX
-		@GetMapping("/fileSelectDel")
+		@PostMapping("/fileSelectDel")
 		@ResponseBody
 		public int fileSelectDel(@RequestParam List<Integer> files) {
 			return fileservice.fileSelDel(files);
