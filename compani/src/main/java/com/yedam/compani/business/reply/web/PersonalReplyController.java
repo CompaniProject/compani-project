263a140548a1fb9deb3cfc5e88cdc163a3944692
gyowoo1113 <yereube@naver.com>
2023-11-10 17:23:29 +0900

    Feat #12 : ajax 활용, 페이징 데이터 받아와서 테이블 재출력
     네비게이션 바 최소/최대 페이지 변경하도록 반영 필요
     td 태그 뱃지/date time format 수정 필요

--- compani/src/main/java/com/yedam/compani/business/reply/web/PersonalReplyController.java
+++ compani/src/main/java/com/yedam/compani/business/reply/web/PersonalReplyController.java
@@ -1,5 +1,6 @@
 package com.yedam.compani.business.reply.web;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,10 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.compani.business.reply.service.BusinessReplyService;
-import com.yedam.compani.business.reply.service.BusinessReplyVO;
-import com.yedam.compani.business.service.BusinessService;
 import com.yedam.compani.member.service.MemberVO;
 import com.yedam.compani.session.service.SessionService;
 
@@ -25,23 +26,42 @@ public class PersonalReplyController {
 
 	private final BusinessReplyService businessReplyService;
 	private final SessionService sessionService;
+	private final Integer AMOUNT = 8;
 	
 	@GetMapping("/personalReply/{prjtNo}")
-	public String personalReplyPage(@PathVariable Integer prjtNo, Model model, HttpServletRequest request) {
+	public String personalReplyPage(@PathVariable Integer prjtNo
+									, @RequestParam(required = false, defaultValue = "1") int pageNum
+									, Model model
+									, HttpServletRequest request) {
 		
+		List<Map<Object,Object>> personalReplys = businessReplyService.getBusinessReply(
+														sessionService.getLoginInfo(request).getMembId()
+														,prjtNo
+														,pageNum
+														,AMOUNT);
+		
+		model.addAttribute("personalReplys", personalReplys);
+		sessionService.setProjectInfo(prjtNo, request);
+		sessionService.setProjectNo(prjtNo, request);
+		
+		return "project/personal-reply";
+	}
+	
+	@GetMapping("personalReply")
+	@ResponseBody
+	public Map<String, Object> getPersonalReplyPage(int pageNum, HttpServletRequest request) {
+		Map<String, Object> map = new HashMap<>();
 		MemberVO memberVO = sessionService.getLoginInfo(request);
-		Integer pageNum = 1;
-		Integer amount = 8;
+		Integer prjtNo = sessionService.getProjectNo(request);
 		
 		List<Map<Object,Object>> personalReplys = businessReplyService.getBusinessReply(
 														memberVO.getMembId()
 														,prjtNo
 														,pageNum
-														,amount);
+														,AMOUNT);
 		
-		model.addAttribute("personalReplys", personalReplys);
-		sessionService.setProjectInfo(prjtNo, request);
+		map.put("replys", personalReplys);
 		
-		return "project/personal-reply";
+		return map;
 	}
 }
