9f29e4d2884b2fc85a90cde7690388f35f5568ce
imojun <kozwns@gmail.com>
2023-11-16 15:30:15 +0900

    filePath 수정

--- compani/src/main/java/com/yedam/compani/member/service/WebMvcConfig.java
+++ compani/src/main/java/com/yedam/compani/member/service/WebMvcConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
-	    @Value("file:///${filePath.File}/")
+	    @Value("file:///${filePath}/")
 	    private String downloadPath;
 	    
 
